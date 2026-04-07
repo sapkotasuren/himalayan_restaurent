f95b6092e944bf10a0b1db5104a2a9fbf99f0e5a
Surendra <surendra@surendras-mbp.home>
2026-04-07 23:22:57 +0200

    trail and error build project to export as stat content

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // enables static export
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
